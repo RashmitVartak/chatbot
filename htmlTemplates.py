0955833442d3ab7fdbcb7a6baefee007557db35d
RashmitVartak <rashmitvartak@gmail.com>
2023-10-18 15:06:49 +0530

    chatbot

---
+++ htmlTemplates.py
@@ -0,0 +1,27 @@
+css = '''
+<style>
+.chat-message {
+    padding: 1.5rem; border-radius: 0.5rem; margin-bottom: 1rem; display: flex
+}
+.chat-message.user {
+    background-color: #2b313e
+}
+.chat-message.bot {
+    background-color: #475063
+}
+.chat-message .avatar {
+  width: 20%;
+}
+'''
+
+bot_template = '''
+<div class="chat-message bot">
+    <div class="message">{{MSG}}</div>
+</div>
+'''
+
+user_template = '''
+<div class="chat-message user">
+    <div class="message">{{MSG}}</div>
+</div>
+'''
